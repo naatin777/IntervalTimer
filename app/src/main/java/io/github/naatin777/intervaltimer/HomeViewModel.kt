a5bd7b63c6f606ed91866eac022187fd9025a574
Naatin <76672482+naatin777@users.noreply.github.com>
2025-08-18 20:36:46 +0900

    Add Fragment and ViewModel boilerplate

---
+++ app/src/main/java/io/github/naatin777/intervaltimer/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.naatin777.intervaltimer
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/io/github/naatin777/intervaltimer/LicenseViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.naatin777.intervaltimer
+
+import androidx.lifecycle.ViewModel
+
+class LicenseViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
