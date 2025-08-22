dea0f53c9f16ff6f724523e5a35c71c92b400f7a
Naatin <76672482+naatin777@users.noreply.github.com>
2025-08-22 22:51:27 +0900

    feat: move to each screen (#28)
    * Add menu click listeners in HomeFragment
    
    * Introduce Navigation Safe Args
    
    * Navigate to SettingsFragment from HomeFragment
    
    * Navigate to AboutFragment from HomeFragment menu
    
    * Add navigation animation to nav_graph.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.devtools.ksp)
     alias(libs.plugins.hilt.android)
+    alias(libs.plugins.androidx.navigation.safeargs)
 }
 
 android {
--- app/src/main/java/io/github/naatin777/intervaltimer/home/HomeFragment.kt
+++ app/src/main/java/io/github/naatin777/intervaltimer/home/HomeFragment.kt
@@ -2,10 +2,20 @@ package io.github.naatin777.intervaltimer.home
 
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
+import io.github.naatin777.intervaltimer.R
 import io.github.naatin777.intervaltimer.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -30,7 +40,35 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.searchBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_sort -> {
+                    Log.d("HomeFragment", "Sort menu item clicked")
+                    true
+                }
+                R.id.menu_backup -> {
+                    Log.d("HomeFragment", "Backup menu item clicked")
+                    true
+                }
+                R.id.menu_settings -> {
+                    val action = HomeFragmentDirections.actionHomeFragmentToSettingsFragment()
+                    findNavController().navigate(action)
+                    true
+                }
+                R.id.menu_about -> {
+                    val action = HomeFragmentDirections.actionHomeFragmentToAboutFragment()
+                    findNavController().navigate(action)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     override fun onDestroyView() {
